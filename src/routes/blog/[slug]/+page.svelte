7a0a0974b2fe2d2fb0ec79870ff385a21737a810
Bruno Chaves <bruno@chaves.fr>
2025-02-04 13:14:23 +0100

    text size 2

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,7 +2,10 @@
 	import type { PageData } from './$types';
 	import Breadcrumb from '$lib/components/layout/Breadcrumb.svelte';
 	import * as runtime from '$lib/paraglide/runtime.js';
+	import * as m from '$lib/paraglide/messages.js';
+
 	import { marked } from 'marked';
+	import PostAuthors from '$lib/components/layout/PostAuthors.svelte';
 
 	let { data }: { data: PageData } = $props();
 	let post = data.post[0];
@@ -29,7 +32,11 @@
 <!-- Container flex layout to place post content on the left and image on the right -->
 <div class="flex flex-col md:flex-row">
 	<div class="blogPost flex-1">
-		{@html contentHtml}
+		<div class="prose max-w-none text-justify">{@html contentHtml}</div>
+        <p class="italic text-gray-500 text-right">
+            {m.published_at()} {localizedPublishedAt} <PostAuthors authors={post.authors} />
+        </p>
+
 	</div>
 	<div class="flex items-start justify-center md:w-1/3 md:pl-4">
 		<img
