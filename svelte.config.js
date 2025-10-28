ba510c1ee05f10852f5fd21d7a9a8e2e78a1e133
Bruno Chaves <bruno@chaves.fr>
2025-10-28 16:54:59 +0100

    prerender

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -6,8 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess()
-	],
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
@@ -17,6 +15,7 @@ const config = {
 		prerender: {
 			//Needed for correctly prerendering <link rel="alternate" hreflang="x" href="y">
 			// origin: "https://github.io",
+			default: true
 		}
 	},
 
